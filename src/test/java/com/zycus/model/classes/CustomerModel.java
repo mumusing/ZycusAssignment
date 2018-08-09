9022ada8ff27ef54a910405559d92896098184fc
Mukesh Singh <mukesh.singh1792@gmail.com>
2018-08-10 01:44:35 +0530

    Adding package com.zycus.testSuites where All the Model classes will be Added. This classes will be used for PayLoad
    CustomerModel.java:-- This class store customer payLoad

---
+++ src/test/java/com/zycus/model/classes/CustomerModel.java
@@ -0,0 +1,58 @@
+package com.zycus.model.classes;
+
+public class CustomerModel {
+	private String name;
+	private String phone_number;
+	private String address;
+	private String website;
+	private String language;
+    private String email;
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPhone_number() {
+		return phone_number;
+	}
+
+	public void setPhone_number(String phone_number) {
+		this.phone_number = phone_number;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getWebsite() {
+		return website;
+	}
+
+	public void setWebsite(String website) {
+		this.website = website;
+	}
+
+	public String getLanguage() {
+		return language;
+	}
+
+	public void setLanguage(String language) {
+		this.language = language;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}
